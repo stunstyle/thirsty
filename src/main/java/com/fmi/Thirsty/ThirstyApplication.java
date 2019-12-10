9065dee8206e0132072506806fd6f8f7c223f021
Ivan Chavdarov <ivan.chavdarov@novarto.com>
2019-12-10 20:25:07 +0200

    intial commit 2

---
+++ src/main/java/com/fmi/Thirsty/ThirstyApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.Thirsty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirstyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirstyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmi/Thirsty/ThirstyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.Thirsty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirstyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
