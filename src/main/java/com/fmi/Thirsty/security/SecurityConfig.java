6cc22ca1bc07d1e930be29ffa1bfa5d7c20cb4b9
stunstyle <stunstyle@yahoo.com>
2019-12-15 21:36:11 +0200

    add CORS filter for development purposes

--- src/main/java/com/fmi/Thirsty/security/SecurityConfig.java
+++ src/main/java/com/fmi/Thirsty/security/SecurityConfig.java
@@ -1,60 +1,63 @@
 package com.fmi.Thirsty.security;
 
-import com.fmi.Thirsty.services.MyUserDetailsService;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.fmi.Thirsty.services.MyUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter
-{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    MyUserDetailsService myUserDetailsService;
+	@Autowired
+	MyUserDetailsService myUserDetailsService;
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception
-    {
-        http.authorizeRequests().antMatchers("/error", "/login", "/logout", "/register/user/**").permitAll()
-                .antMatchers("/user/{username}/**")
-                .access("@userSecurity.hasUserAuthority(authentication, #username)")
-                .anyRequest().authenticated().and()
-                .formLogin().and().httpBasic();
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/error", "/login", "/logout", "/register/user/**").permitAll()
+				.antMatchers("/user/{username}/**").access("@userSecurity.hasUserAuthority(authentication, #username)")
+				.anyRequest().authenticated().and().formLogin().and().httpBasic();
 
-        http.cors().disable().csrf().disable();
-    }
+		http.cors().disable().csrf().disable();
+	}
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
-        auth.userDetailsService(myUserDetailsService);
-    }
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(myUserDetailsService);
+	}
 
-    @Bean
-    PasswordEncoder getPasswordEncoder()
-    {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-}
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:3000");
+		config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
+		config.setAllowedHeaders(
+				Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 
-@Configuration
-@EnableWebMvc
-class MvcConfig extends WebMvcConfigurerAdapter
-{
-    @Override
-    public void addCorsMappings(CorsRegistry registry)
-    {
-        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-    }
 }
